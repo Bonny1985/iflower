96a99b6dd3eed7eddf112719ff87ce330b6c0859
Luca Bonaldo <lbonaldo@bancaetica.com>
2022-04-01 13:00:45 +0200

    init

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:iflower/Screens/InsertScreen.dart';
 import 'package:iflower/Screens/SearchScreen.dart';
 
+/// Schermata principale
 class HomeScreen extends StatelessWidget {
 
   const HomeScreen({Key? key}) : super(key: key);
@@ -9,11 +10,12 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
    
-    // stile dei pulsanti
+    // stile comune dei pulsanti
     final ButtonStyle style =
         ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
 
-    Widget btnInsert = ElevatedButton(
+    // pulsante per inserimento
+    ElevatedButton btnInsert = ElevatedButton(
         style: style,
         child: const Text('AGGIUNGI FIORE'),
         onPressed: () {
@@ -24,7 +26,8 @@ class HomeScreen extends StatelessWidget {
         }
     );
 
-    Widget btnSearch = ElevatedButton(
+    // pulsante per ricerca
+    ElevatedButton btnSearch = ElevatedButton(
         style: style, 
         child: const Text('CERCA FIORE'), 
         onPressed: () {
@@ -35,38 +38,29 @@ class HomeScreen extends StatelessWidget {
         }
     );
 
+    // colonna per renderizzare i pulsanti in serie
+    Column col = Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        btnInsert,
+        const SizedBox(height: 30), // spazio tra i pulsanti
+        btnSearch
+      ],
+    );
+
+    // Riga per per allineare al centro dello schermo i pulsanti
+    Row row = Row(
+      mainAxisAlignment: MainAxisAlignment.center, 
+      children: [col]
+    );
+
     return SafeArea(
         child: Scaffold(
-            appBar: AppBar(
-              title: const Text("iFlower App")
-            ),
-            body: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    btnInsert,
-                    const SizedBox(height: 30), // spazio tra i pulsanti
-                    btnSearch
-                  ],
-                ),
-              ],
-            )));
+            appBar: AppBar(title: const Text("iFlower App")),
+            body: row
+          )
+    );
+
+
   }
 }
-/*
-
-                        setState(() {
-                          counter++;
-                        });
-
-                        Flower f = Flower(
-                            color: FlowerColor.RED,
-                            name: "Name $counter",
-                            description: "Descr $counter",
-                            size: FlowerSize.MEDIUM
-                        );
-
-                        _repository.insert(f);
-*/
